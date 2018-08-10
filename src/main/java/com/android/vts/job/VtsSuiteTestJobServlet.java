91647f1bc86aa3eba3f6d553a97cb61dfccce4be
Young Gyu Park <younggyu@google.com>
2018-08-10 13:16:19 +0900

    Adding null check logic for suite test name
    Test: go/vts-web-staging
    Bug: 112338819
    Change-Id: I8ced065c879354c3946113d331ee9cc8eb52e281

--- src/main/java/com/android/vts/job/VtsSuiteTestJobServlet.java
+++ src/main/java/com/android/vts/job/VtsSuiteTestJobServlet.java
@@ -143,77 +143,70 @@ public class VtsSuiteTestJobServlet extends HttpServlet {
         }
         dateStringList.add(todayDateString);
 
-        dateStringList.forEach(
-                dateString -> {
-                    String[] dateArray = dateString.split("-");
-                    if (dateArray.length == 3) {
+        for (String dateString : dateStringList) {
+            String[] dateArray = dateString.split("-");
+            if (dateArray.length == 3) {
 
-                        Queue queue = QueueFactory.getQueue(QUEUE);
+                Queue queue = QueueFactory.getQueue(QUEUE);
 
-                        List<TaskOptions> tasks = new ArrayList<>();
+                List<TaskOptions> tasks = new ArrayList<>();
 
-                        String fileSeparator = FileSystems.getDefault().getSeparator();
+                String fileSeparator = FileSystems.getDefault().getSeparator();
 
-                        String year = dateArray[0];
-                        String month = dateArray[1];
-                        String day = dateArray[2];
+                String year = dateArray[0];
+                String month = dateArray[1];
+                String day = dateArray[2];
 
-                        List<String> pathList =
-                                Arrays.asList(GCS_SUITE_TEST_FOLDER_NAME, year, month, day);
-                        Path pathInfo = Paths.get(String.join(fileSeparator, pathList));
+                List<String> pathList = Arrays.asList(GCS_SUITE_TEST_FOLDER_NAME, year, month, day);
+                Path pathInfo = Paths.get(String.join(fileSeparator, pathList));
 
-                        List<TestSuiteFileEntity> testSuiteFileEntityList =
-                                ofy().load()
-                                        .type(TestSuiteFileEntity.class)
-                                        .filter("year", Integer.parseInt(year))
-                                        .filter("month", Integer.parseInt(month))
-                                        .filter("day", Integer.parseInt(day))
-                                        .list();
+                List<TestSuiteFileEntity> testSuiteFileEntityList =
+                        ofy().load()
+                                .type(TestSuiteFileEntity.class)
+                                .filter("year", Integer.parseInt(year))
+                                .filter("month", Integer.parseInt(month))
+                                .filter("day", Integer.parseInt(day))
+                                .list();
 
-                        List<String> filePathList =
-                                testSuiteFileEntityList
-                                        .stream()
-                                        .map(testSuiteFile -> testSuiteFile.getFilePath())
-                                        .collect(Collectors.toList());
+                List<String> filePathList =
+                        testSuiteFileEntityList
+                                .stream()
+                                .map(testSuiteFile -> testSuiteFile.getFilePath())
+                                .collect(Collectors.toList());
 
-                        Bucket vtsReportBucket = this.storage.get(GCS_BUCKET_NAME);
+                Bucket vtsReportBucket = this.storage.get(GCS_BUCKET_NAME);
 
-                        Storage.BlobListOption[] listOptions =
-                                new Storage.BlobListOption[] {
-                                    Storage.BlobListOption.prefix(
-                                            pathInfo.toString() + fileSeparator)
-                                };
+                Storage.BlobListOption[] listOptions =
+                        new Storage.BlobListOption[] {
+                            Storage.BlobListOption.prefix(pathInfo.toString() + fileSeparator)
+                        };
 
-                        Iterable<Blob> blobIterable =
-                                vtsReportBucket.list(listOptions).iterateAll();
-                        Iterator<Blob> blobIterator = blobIterable.iterator();
-                        while (blobIterator.hasNext()) {
-                            Blob blob = blobIterator.next();
-                            if (blob.isDirectory()) {
-                                logger.log(
-                                        Level.INFO, blob.getName() + " directory will be skipped!");
-                            } else {
-                                if (filePathList.contains(blob.getName())) {
-                                    logger.log(
-                                            Level.INFO,
-                                            "filePathList contain => " + blob.getName());
-                                } else if (blob.getName().endsWith(fileSeparator)) {
-                                    logger.log(Level.INFO, blob.getName() + " endswith slash!");
-                                } else {
-                                    TaskOptions task =
-                                            TaskOptions.Builder.withUrl(SUITE_TEST_URL)
-                                                    .param("filePath", blob.getName())
-                                                    .method(TaskOptions.Method.POST);
-                                    tasks.add(task);
-                                }
-                            }
-                        }
-                        TaskQueueHelper.addToQueue(queue, tasks);
+                Iterable<Blob> blobIterable = vtsReportBucket.list(listOptions).iterateAll();
+                Iterator<Blob> blobIterator = blobIterable.iterator();
+                while (blobIterator.hasNext()) {
+                    Blob blob = blobIterator.next();
+                    if (blob.isDirectory()) {
+                        logger.log(Level.INFO, blob.getName() + " directory will be skipped!");
                     } else {
-                        throw new IllegalArgumentException(
-                                todayDateString + " date string not in correct format");
+                        if (filePathList.contains(blob.getName())) {
+                            logger.log(Level.INFO, "filePathList contain => " + blob.getName());
+                        } else if (blob.getName().endsWith(fileSeparator)) {
+                            logger.log(Level.INFO, blob.getName() + " endswith slash!");
+                        } else {
+                            TaskOptions task =
+                                    TaskOptions.Builder.withUrl(SUITE_TEST_URL)
+                                            .param("filePath", blob.getName())
+                                            .method(TaskOptions.Method.POST);
+                            tasks.add(task);
+                        }
                     }
-                });
+                }
+                TaskQueueHelper.addToQueue(queue, tasks);
+            } else {
+                throw new IllegalArgumentException(
+                        todayDateString + " date string not in correct format");
+            }
+        }
     }
 
     @Override
@@ -245,12 +238,8 @@ public class VtsSuiteTestJobServlet extends HttpServlet {
 
                 if (blobFile.exists()) {
                     TestSuiteFileEntity newTestSuiteFileEntity = new TestSuiteFileEntity(filePath);
-                    newTestSuiteFileEntity.save();
-                    TestSuiteResultMessageProto.TestSuiteResultMessage testSuiteResultMessage;
                     try {
-                        // String payload = blobFile.getContent().collect(Collectors.joining());
-                        // byte[] value = Base64.decodeBase64(payload);
-                        testSuiteResultMessage =
+                        TestSuiteResultMessageProto.TestSuiteResultMessage testSuiteResultMessage =
                                 TestSuiteResultMessageProto.TestSuiteResultMessage.parseFrom(
                                         blobFile.getContent());
 
@@ -278,7 +267,7 @@ public class VtsSuiteTestJobServlet extends HttpServlet {
                                         testSuiteResultMessage.getPassedTestCaseCount(),
                                         testSuiteResultMessage.getFailedTestCaseCount());
 
-                        testSuiteResultEntity.save();
+                        testSuiteResultEntity.save(newTestSuiteFileEntity);
                     } catch (IOException e) {
                         ofy().delete().type(TestSuiteFileEntity.class).id(filePath).now();
                         response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
